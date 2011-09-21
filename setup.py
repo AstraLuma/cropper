378498b5e08dcd3ade5b15bdea47b7e47fec52b3
James Bliss <james.bliss@astro73.com>
2011-09-21 00:27:08 -0400

    Metadata updates to setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,29 @@ from distutils.core import setup
 import croppertools.cropper
 
 setup(
-	name='Cropper',
+	name='cropper',
 	version=croppertools.cropper.__version__,
 	description='Simple image slicing program',
 	long_description=open('README.rst').read(),
 	author='James Bliss',
 	author_email='james.bliss@astro73.com',
-	url='https://github.com/astronouth7303/cropper',
+	url='https://astronouth7303.github.com/cropper',
 	packages=['croppertools'],
 	requires=['pygtk', 'PIL'],
 	scripts=['cropper'],
 	data_files=[('share/cropper', ['share/cropper.ui', 'share/drag-resize.png'])],
-     )
+	download_url='https://github.com/astronouth7303/cropper/tarball/v'+croppertools.cropper.__version__,
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Environment :: X11 Applications :: GTK',
+		'Intended Audience :: End Users/Desktop',
+		'License :: OSI Approved :: GNU General Public License (GPL)',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Topic :: Artistic Software',
+		'Topic :: Multimedia :: Graphics',
+		'Topic :: Multimedia :: Graphics :: Editors',
+		'Topic :: Utilities',
+		],
+	)
